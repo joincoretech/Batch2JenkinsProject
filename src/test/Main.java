5c123e116520f9f52b780d1e3028a4a6d3590522
azizullah.ayoub <azizullahayoubi19@gmail.com>
2023-11-19 11:55:55 -0500

    second commit

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("this is my first code");
+        System.out.println("this is my second code");
+        System.out.println("this is my second code");
+
     }
 }
